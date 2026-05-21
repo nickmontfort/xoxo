3caef7c7a926cdb6a59ac3e69218a094fb0695bd
Nick Montfort <nickm@nickm.com>
2026-05-21 09:13:49 -0400

    Bug fixes, add commentary on opening move & any blocking moves

--- xoxo.py
+++ xoxo.py
@@ -16,49 +16,62 @@ class Board():
         self.grid = []
         for i in [0, 1, 2]:
             self.grid.append(["_", "_", "_"])
-    def spaces_are_free(self):
-        slots = False
-        for i in [0, 1, 2]:
-            if "_" in self.grid[i]:
-                slots = True
-        return slots
+        self.move_num = 0
+    def other(self, current):
+        return "X" if current == "O" else "O"
     def display(self):
         for i in [2, 1, 0]:
             print(i+1, " ".join(self.grid[i]))
         print("  a b c", end="\n\n")
     def place(self, symbol, i, j):
-        if self.grid[i][j] == "_":
-            self.grid[i][j] = symbol
-        return self.grid[i][j] == symbol
-    def move_won_game(self, current, i, j):
-        unit = current * 3
-        won = "".join(self.grid[i]) == unit \
+        move_type = 'normal'
+        if not self.grid[i][j] == "_":
+            return False
+        elif self.move_blocks_opponent(current, i, j):
+            move_type = 'blocking'
+        self.grid[i][j] = symbol # Overwrites move_blocks_opponent()
+        self.move_num += 1
+        return move_type
+    def move_blocks_opponent(self, symbol, i, j):
+        self.grid[i][j] = self.other(symbol) # Will be overwritten in place()
+        return self.move_wins_game(self.other(symbol), i, j)
+    def move_wins_game(self, symbol, i, j):
+        unit = symbol * 3
+        return "".join(self.grid[i]) == unit \
             or self.grid[0][j] + self.grid[1][j] + self.grid[2][j] == unit \
             or self.grid[0][0] + self.grid[1][1] + self.grid[2][2] == unit \
             or self.grid[0][2] + self.grid[1][1] + self.grid[2][0] == unit
-        return won
         
-current = input("Who will play first, X or O? ").upper()
+current = ""
+while current not in ["X", "O"]:
+    current = input("Who will play first, X or O? ").upper()
 print()
 
 board = Board()
-
-while board.spaces_are_free():
+while board.move_num < 9:
     board.display()
     move = ""
     while not len(move) == 2 or move[0] not in "abc" or move[1] not in "123":
         move = input(f"{current}, make a move in the format a1: ")
     i = int(move[1])-1
     j = ord(move[0])-97
-    if not board.place(current, i, j):
-        print("That space is occupied.")
+    move_type = board.place(current, i, j)
+    if not move_type:
+        print("That space is occupied.", end="\n\n")
         continue
-    elif board.move_won_game(current, i, j):
+    elif board.move_wins_game(current, i, j):
         break
-    current = "X" if current == "O" else "O" 
+    comment, extra = f"{current} takes {move}.", ""
+    if board.move_num == 1:
+        extra = "Good move!" if move == 'b2' \
+            else "There must have been a better move?"
+    elif move_type == 'blocking':
+        extra = f"{current} blocked {board.other(current)} from a winning move!"
+    print(" ".join([comment, extra]), end="\n\n")
+    current = board.other(current) 
 
 board.display()
-if board.move_won_game(current, i, j):
+if board.move_wins_game(current, i, j):
     print(f"{current} has won!", end="\n\n")
-elif not board.spaces_are_free():
+elif not board.move_num == 9:
     print("It's a draw.", end="\n\n")
